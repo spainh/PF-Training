511c91dcd93556ac7934414914e3f6e3b97f5934
Harrison Spain <spainh@sunypoly.edu>
2023-02-24 04:49:51 -0500

    customized plans cards a bit further

--- frontend/src/components/Plan.js
+++ frontend/src/components/Plan.js
@@ -1,21 +1,23 @@
 import React from "react"
 import { Card,Button } from "react-bootstrap"
+import CardHeader from "react-bootstrap/esm/CardHeader"
 // pass in plan from plan.js so this component knows what it is (descructuring)
 const Plan = ({ plan }) => {
   return (
     <>
       {/* margin 3 padding 3 and rounded */}
-      <Card className='m-3 p-3 rounded planCard'>
+      <Card className='m-3 p-3 rounded planCard border-primary'>
         {/* may not need but for specifying each plan (for purchasing) */}
         {/* <Link to={`/Plan/${plan._id}`}/> */}
         <Card.Body>
           {/* will put plans name in a div and display */}
           <Card.Title as='h3'>
-            <>{plan.name}</>
+            {/* <div className="card-header">{plan.name}</div> */}
+            <div className="">{plan.name}</div>
           </Card.Title>
           {/* description of each plan (can edit later to add benefits maybe) */}
           <Card.Text as='div'>
-            <div className='my-1'><em>{plan.description}</em></div>
+            <div className='my-1 '><em>{plan.description}</em></div>
           </Card.Text>    
           {/* each plans price */}
           <Card.Text as='h3'>
