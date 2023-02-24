2eb6a1542d3ebc7c25c4e93d7085e97608152fa1
Harrison Spain <spainh@sunypoly.edu>
2023-02-24 04:25:30 -0500

    Found a icon that works nothing too special though

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,21 +4,16 @@ import frontPic from "../sample_images/CBum.jpg"
 const Home = () => {
   return (
     <section>
-    <Row className="ms-auto">
-        <Col><h2><em>When you Train Right</em></h2></Col>
-        <row>
-
-        </row>
-    </Row>
-    <Row className="ms-auto">
-    <h2 className="statement"><em>You see result's</em></h2>
-    <div className="text-center">
-    <img src={frontPic} alt= "frontpic"></img>
-    </div>
-        <Col className="text-center">
-        </Col>
-    </Row>
-  </section>
+      <Container>
+        <div className='ms-auto py-3'>
+          <h2><em>When you Train Right</em></h2>
+            <h2 className=''><em>&nbsp;You see result's</em></h2>
+            <div className='text-center'>
+              <img src={frontPic} alt='frontpic'></img>
+            </div>
+        </div>
+      </Container>
+    </section>
   )
 }
 
