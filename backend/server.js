7096fc41a92ff5a74023e7e3839a2ee63a29cdf9
Harrison Spain <spainh@sunypoly.edu>
2023-03-22 15:11:58 -0400

    Added Middleware and get request functionality

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@
 import express from "express"
 // bring in dotenv
 import dotenv from "dotenv"
-import plans from "./data/plans.js"
 import connectDB from './config/db.js'
+import planRoutes from './routes/planRoutes.js'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 dotenv.config()
 
@@ -14,17 +15,13 @@ const app = express()
 app.get("/", (req, res) => {
   res.send("API is running...")
 })
-// route to get plans
-app.get("/api/plans", (req, res) => {
-  res.json(plans)
-})
-// route to get plan by id
-app.get("/api/plans/:id", (req, res) => {
-  // get single product
-  const plan = plans.find((p) => p._id === req.params.id)
-  //   return single product
-  res.json(plan)
-})
+
+// anything that goes to /api/products will be linked to
+// productedRoutes...
+app.use('/api/plans', planRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
